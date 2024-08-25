9e7c93c75ccbb3ed7037dd3d2a8b9d2b23e13d78
kloimhardt <markus.agwin@gmx.net>
2024-08-25 16:44:50 +0200

    calc Dulong et Petit Formel

--- stefan.clj
+++ stefan.clj
@@ -11,15 +11,6 @@
   )
 
 ^:kindly/hide-code
-(comment
-  ;; clj -Sdeps "{:deps {org.scicloj/clay {:mvn/version \"2-beta15\"}}}"
-  (do
-    (require '[scicloj.clay.v2.api :as clay])
-    (clay/make! {:source-path "babashka-snipets/stefan.clj"})
-    )
-  )
-
-^:kindly/hide-code
 (ns stefan
   (:require [scicloj.kindly.v4.api :as kindly]
             [scicloj.kindly.v4.kind :as kind]
@@ -30,10 +21,37 @@
   (comp kindly/hide-code kind/md))
 
 (defn postfix? [e]
-  (= (str (last e)) "stel"))
+  (#{"stel"}
+    (str (last e))))
 
 (defn infix? [e]
-  true)
+  (#{"plus" "minus" "mal" "von" "dot" "durch" "hoch" "Ein" "Milliard"
+     "Komma"}
+    (str (second e))))
+
+(defn npow [x n]
+  (apply * (repeat n x)))
+
+(defn mypow [x n]
+  (if (integer? n)
+    (npow x n)
+    (clojure.math/pow x n)))
+
+(def plus +)
+(def minus -)
+(def mal *)
+(def von *)
+(def dot *)
+(def durch /)
+(def hoch mypow)
+(def null 0)
+(def Ein 1)
+(def e (clojure.math/exp 1))
+(def Milliard (fn [x] (* x (mypow 10 9))))
+(def stel (fn [x y] (/ x y)))
+(defn Komma [& lst]
+  (Double/parseDouble (apply str (conj (rest lst) "." (first lst)))))
+
 
 (defn sw [e]
   (cond
@@ -49,29 +67,24 @@
 (defn sw3 [e]
   (clojure.walk/postwalk sw2 e))
 
-(defn bx [e]
-  (str "\\fbox{" (apply str (interpose " " e)) "}"))
+(defn bx [ex]
+  (str "\\fbox{" (apply str (interpose " " ex)) "}"))
 
 (defn bx2 [e]
-  (if (coll? e) (bx e) e))
+  (if (coll? e)
+    (if (= (str (second e)) "dot")
+      (bx (conj (rest (rest e)) (first e)))
+      (bx e))
+    e))
 
 (defn bx3 [e]
   (clojure.walk/postwalk bx2 e))
 
 (defmacro calcbox [e & dbg]
   (let [swe (sw3 e)]
-       {:code `'~swe
-        :calc (if (seq dbg) (str dbg) swe)
-        :tex (bx3 e)}))
-
-(defn mypow [x n]
-  (apply * (repeat n x)))
-
-(def hoch mypow)
-(def plus +)
-(def minus -)
-(def mal *)
-(def durch /)
+    {:code `'~swe
+     :calc (if (seq dbg) (str dbg) swe)
+     :tex  (bx3 e)}))
 
 (md "# Start now")
 
@@ -92,25 +105,31 @@
   (apply mapv vector m))
 
 ^:kindly/hide-code
-(defn tenpow [n]
-  (apply * (repeat n 10)))
-
-^:kindly/hide-code
 (defn round [x dec]
-  (double (/ (math/round (* x (tenpow dec))) (tenpow dec))))
+  (double (/ (math/round (* x (mypow 10 dec))) (mypow 10 dec))))
 
-;; Unten die Werte in Tabellenform, die berechnete Differenz passt mit Stefan-Paper überein
+(defn dp-formel [X]
+  (calcbox (2 mal ((e hoch ((0 Komma 0 0 767) dot X)) minus 1))))
+
+;; X ist die Temperatur in Grad Celsius. Man muss wissen wo das X steh, welche Werte es ungefähr haben kann und wie man die Formel von innen nach aussen lesen muss, vom X her.
+
+;; eigentlich 2.02 statt genau 2, aber auch mit 2.02 komm ich nur auf ein zwei zehntel hin.
+;; 0.00767 ist ln(1.0077), weil formel im paper ist 1,0077^T, habs auf exp umgeändert
+
+(kind/tex (:tex (dp-formel 0)))
+
+(clojure.math/log 1.0077)
+
+e
+
+;; Unten die Werte in Tabellenform, die berechnete Differenz D&P-Stefan passt mit Paper überein, meine Nachrechnung D&P passt nicht ganz.
+
 (def tb2 (map (fn [[T m t]]
-                [T m (round (- t m) 2) t])
+                [T m (round (- t m) 2) t (round (:calc (dp-formel T)) 2)])
               (transpose [temper dp-meas dp-theor])))
 
-;; Temperatur Messwert Differenz RechenWert_D&P
+;; Temperatur Messwert Differenz RechenWert_D&P Meine-Rechnung
 tb2
-
-(def Ein 1)
-(def Milliard (fn [x] (* x (tenpow 9))))
-(def stel (fn [x y] (/ x y)))
-(def von *)
 
 (defn stefan-formel [X]
   (calcbox ((Ein (6 Milliard) stel)
@@ -122,11 +141,19 @@ tb2
 (kind/tex (:tex (stefan-formel 0)))
 (kind/md "So eine Formel kann man immer auf zwei Arten lesen. Einml ganz normal. Probiers. Ein sechs Milliardstel von X plus 273 hoch 4 minus 273 hoch vier. Lesen kann jeder.")
 
-(mypow 273 4)
-
 (kind/md "Man sollte die Formel aber im zweiten Schritt immer auch vom X her lesen. Von innen heraus quasi. X plus 273, das ganze hoch 4, dann das ganze minus ca. 5 1/2 Milliarden und das ganze dann durch 6 Milliarden.")
 
 (kind/md "Ich könnt mir vorstellen, dass diese Milliarden auch Dulong nd Petit abgeschreckt haben")
+
+(def pow_273_4 (calcbox (273 mal (273 mal (273 mal 273)))))
+
+(kind/tex (:tex pow_273_4))
+
+(:calc pow_273_4)
+
+(kind/md (str "Um genau zu sein, 273 hoch 4 ist folgende Zahl: " (mypow 273 4) ". Das musste man alles mit der Hand rechnen, sodass es wohl kein Wunder ist, dass es bis zu dem T^4 Gesetz so lange gedauert hat. D&P hat zwar Kommas, aber die Grössenordnungen sind im menschlichen Bereich. In der Moderne nicht mehr möglich weil Loschmidtzahl so hoch"))
+
+(kind/md "drum sagt er auch -genauso einfache Formel wie D&P- denn die Milliarden machen's nicht unbedingt einfacher. Heutzutage sagen wir ein einfaches T^4 Gesetz")
 
 (:code (stefan-formel 0))
 
