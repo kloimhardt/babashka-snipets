70cf9708307e0ac0f79efa75833cca55cf7383a8
kloimhardt <markus.agwin@gmx.net>
2024-08-30 13:39:48 +0200

    add Maria+Emmy to the mix

--- mariastefan.clj
+++ mariastefan.clj
@@ -1,15 +1,13 @@
-^:kindly/hide-code
 (ns stefan
-  (:require [scicloj.kindly.v4.api :as kindly]
-            [scicloj.kindly.v4.kind :as kind]
-            [clojure.math :as math]))
+  (:refer-clojure :exclude [+ - * / = abs compare zero? ref partial
+                            numerator denominator infinite?])
+  (:require [emmy.env :as e :refer :all :exclude [F->C]]
+            [scicloj.kindly.v4.api :as kindly]
+            [scicloj.kindly.v4.kind :as kind]))
 
-^:kindly/hide-code
-(def md
-  (comp kindly/hide-code kind/md))
+(def html kind/hiccup)
 
-(md "# Code Präambel (bitte runterscrollen zum Hauptteil)")
-
+;; # Code Präambel (bitte runterscrollen zum Hauptteil)
 (defn postfix? [ctx ex]
   ((-> ctx :Schlusselworte :postfix)
    (str (last ex))))
@@ -41,13 +39,27 @@
 (defn mypow [x n]
   (if (integer? n)
     (npow x n)
-    (clojure.math/pow x n)))
+    (exp (* (log x) n))))
 
 (defn n-transpose [m]
   (apply mapv vector m))
 
+(defn r-transpose [x]
+  (reverse (n-transpose x)))
+
+(defn mround [x] (floor (+ x 0.5)))
+
+(defn to-double [x]
+  (let [n (exp -30)]
+    (- (+ x n) n)))
+
 (defn round [x dec]
-  (double (/ (math/round (* x (mypow 10 dec))) (mypow 10 dec))))
+  (to-double (/ (mround (* x (mypow 10 dec))) (mypow 10 dec))))
+
+(defn str-to-int [i]
+  ;; in maria.cloud
+  #_(int i)
+  (Integer/parseInt i))
 
 (def plus +)
 (def minus -)
@@ -61,7 +73,8 @@
 (def Milliard (fn [x] (* x (mypow 10 9))))
 (def stel (fn [x y] (/ x y)))
 (defn Komma [& lst]
-  (Double/parseDouble (apply str (conj (rest lst) "." (first lst)))))
+  (let [a (apply str (rest lst))]
+    (to-double (+ (first lst) (/ (str-to-int a) (mypow 10 (count a)))))))
 
 (defn sw [ctx ex]
   (cond
@@ -125,29 +138,43 @@
             :notext
             #{"dot"}}
            :Schulwissen
-           {:e  clojure.math/E
-            :pi clojure.math/PI}})
+           {:e  euler
+            :pi pi}})
+
+(defn my-walk
+  [inner outer form]
+  (cond
+    (list? form)      (outer (apply list (map inner form)))
+    (map-entry? form) form
+    (seq? form)       (outer (doall (map inner form)))
+    (record? form)    (outer (reduce (fn [r x] (conj r (inner x))) form form))
+    (coll? form)      (outer (into (empty form) (map inner form)))
+    :else             (outer form)))
+
+(defn postwalk
+  [f form]
+  (my-walk (partial postwalk f) f form))
 
 (defmacro calcbox [ex & dbg]
-  (let [swe (clojure.walk/postwalk #(sw gctx %) (threadzero gctx ex))
+  (let [swe (postwalk #(sw gctx %) (threadzero gctx ex))
         bxe (if (vector? ex)
               (->> (cons (first ex) (apply concat (rest ex)))
-                   (map (fn [mex] (clojure.walk/postwalk #(bx gctx %) mex)))
+                   (map (fn [mex] (postwalk #(bx gctx %) mex)))
                    (interpose " \\\\ ")
                    (apply str))
-              (clojure.walk/postwalk #(bx gctx %) ex))
+              (postwalk #(bx gctx %) ex))
         hxe (if (vector? ex)
               (->> (cons (first ex) (apply concat (rest ex)))
-                   (map (fn [mex] (clojure.walk/postwalk #(hx gctx %) mex)))
+                   (map (fn [mex] (postwalk #(hx gctx %) mex)))
                    (map #(vector :p [:div %]))
                    (into [:p]))
-              [:p (clojure.walk/postwalk #(hx gctx %) ex)])]
-    {:code `'~swe
-     :calc (if (seq dbg) (into [] dbg) swe)
-     :tex  bxe
+              [:p (postwalk #(hx gctx %) ex)])]
+    {:code   `'~swe
+     :calc   (if (seq dbg) (into [] dbg) swe)
+     :tex    bxe
      :hiccup hxe}))
 
-(md "# J. Stefan: Über die Beziehung zwischen der Wärmestrahlung und der Temperatur")
+;; # J. Stefan: Über die Beziehung zwischen der Wärmestrahlung und der Temperatur
 
 ;;Temperatur in Celsius
 (def temper [80 100 120 140 160 180 200 220 240])
@@ -167,62 +194,31 @@
              [x in Celsius])
             [und [(1 Komma 0 0 77) für A]]
             [und [(2 Komma 0 2) für M]]]))
-
-;; X ist die Temperatur in Grad Celsius. Man muss wissen wo das X steh, welche Werte es ungefähr haben kann und wie man die Formel von innen nach aussen lesen muss, vom X her.
-
-;; eigentlich fittet 2.02 besser statt genau 2 (im Paper selbst bisher keinen Wert gefunden), aber auch mit 2.02 komm ich nur auf ein zwei zehntel hin.
-;; 0.00767 ist ln(1.0077), weil formel im paper ist 1,0077^T, habs auf exp umgeändert
-
-(kind/tex (:tex (dp-formel 0)))
-
-(kind/hiccup (:hiccup (dp-formel 0)))
-
-(dp-formel 0)
-
-(clojure.math/log 1.0077)
-
-(def Schulwissen (-> gctx :Schulwissen))
-(:e Schulwissen)
-
-;; Unten die Werte in Tabellenform, die berechnete Differenz D&P-Stefan passt mit Paper überein, meine Nachrechnung D&P passt nicht ganz.
+(html (:hiccup (dp-formel 0)))
 
 (def tb2 (map (fn [[T m t]]
                 [T m (round (- t m) 2) t (round (:calc (dp-formel T)) 2)])
               (n-transpose [temper dp-meas dp-theor])))
 
 ;; Temperatur Messwert Differenz RechenWert_D&P Meine-Rechnung
+
 tb2
 
 (defn stefan-formel [Celsius]
-  (calcbox [(((((T plus x) hoch 4 )
+  (calcbox [((((T plus x) hoch 4 )
                minus
                (T hoch 4))
               mal
               B)
-             mit
-             [x in Celsius])
+            [mit [x in Celsius]]
             [und [(Ein (6 Milliard) stel) für B]]
             [und [273 für T]]]))
-
-(kind/tex (:tex (stefan-formel 0)))
-
-(md "So eine Formel kann man immer auf zwei Arten lesen. Einml ganz normal. Probiers. Ein sechs Milliardstel von X plus 273 hoch 4 minus 273 hoch vier. Lesen kann jeder.")
-
-(md "Man sollte die Formel aber im zweiten Schritt immer auch vom X her lesen. Von innen heraus quasi. X plus 273, das ganze hoch 4, dann das ganze minus ca. 5 1/2 Milliarden und das ganze dann durch 6 Milliarden.")
-
-(md "Ich könnt mir vorstellen, dass diese Milliarden auch Dulong nd Petit abgeschreckt haben, der einser in d&p wird zu 5milliarden bei stefan.")
+(html (:hiccup (stefan-formel 0)))
 
 (def pow_273_4 (calcbox [(((T mal T) mal T) mal T) [mit [T gleich 273]]]))
-
-(kind/tex (:tex pow_273_4))
+(html (:hiccup pow_273_4))
 
 (:calc pow_273_4)
-
-(md (str "Um genau zu sein, 273 hoch 4 ist folgende Zahl: " (mypow 273 4) ". Das musste man alles mit der Hand rechnen, sodass es wohl kein Wunder ist, dass es bis zu dem T^4 Gesetz so lange gedauert hat. D&P hat zwar Kommas, aber die Grössenordnungen sind im menschlichen Bereich. In der Moderne nicht mehr möglich weil Loschmidtzahl so hoch"))
-
-(md "drum sagt er auch -genauso einfache Formel wie D&P- denn die Milliarden machen's nicht unbedingt einfacher. Heutzutage sagen wir ein einfaches T^4 Gesetz")
-
-(:code (stefan-formel 0))
 
 ;; Unten passt die Differenz mit Paper überein, auch Meine-Rechnung lt. Paper gerechnet passt
 (def tb7 (map (fn [[T m st-t]]
@@ -234,14 +230,10 @@ tb2
 tb7
 
 (defn minsec [sec]
-  (mapv math/round [(quot sec 60) (mod sec 60)]))
+  (mapv mround [(quot sec 60) (mod sec 60)]))
 
 (defn speed->sec [sp]
   (* (/ 20.0 sp) 60))
-
-^:kindly/hide-code
-(defn r-transpose [x]
-  (reverse (n-transpose x)))
 
 (defn tb3 [tbl]
   (map (fn [tb]
@@ -252,7 +244,6 @@ tb7
 
 (tb3 (r-transpose [dp-meas dp-theor st-theor]))
 
-(def Grad 'Grad)
 (def Minuten 'Minuten)
 (def Minute 'Minute)
 (def Sekunden 'Sekunden)
@@ -298,40 +289,47 @@ tb7
 ;; Differenz der Sekunden: St-Rechnung Differenz D&P-Messung
 (tb6 (tb3 (r-transpose [dp-meas st-theor])))
 
-(md "# Kleine Fingerübungen")
+;; # Kleine Fingerübungen
 
 (def bsp0 (calcbox (1 plus 3)))
-(kind/tex (:tex bsp0))
+(html (:hiccup bsp0))
+
 (:calc bsp0)
+
 ;; praktisch zum debuggen: der generierte code
 (:code bsp0)
-(kind/hiccup (:hiccup bsp0))
 
 (def bsp1 (calcbox (2 plus 1) "andere Rechnung" (+ 4 5)))
-(kind/tex (:tex bsp1))
+(html (:hiccup bsp1))
+
 ;; noch praktischer zum debuggen: calculation unterdrücken
 (:calc bsp1)
 
 (def bsp2 (calcbox ((X plus 1) mit [X gleich [(Y plus 2) mit [Y gle=ich 3]]])))
-(kind/tex (:tex bsp2))
+(html (:hiccup bsp2))
+
 (:calc bsp2)
 
 (def bsp3 (calcbox ( [(X plus Y) mit [X ist-gleich 3]] mit [Y ist 2]) ))
 ;; es ist wurst ob glei=ch, ist-gleich, -in-: nur ein Füllwort
-(kind/tex (:tex bsp3))
+(html (:hiccup bsp3))
+
 (:calc bsp3)
 
 (defn bsp4 [Sekunden Y]
   (calcbox ((X plus Y) mit [X -in- Sekunden])))
 
-(kind/tex (:tex (bsp4 0 0)))
+(html (:hiccup (bsp4 0 0)))
+
 (:calc (bsp4 5 6))
 
 (defn bsp5 [{:keys [Schulwissen]}]
   (calcbox ((e hoch pi) mit [[e pi] aus Schulwissen])))
-(kind/tex (:tex (bsp5 gctx)))
+(html (:hiccup (bsp5 gctx)))
+
 (bsp5 gctx)
 
 (def bsp6 (calcbox [(X plus Y) [mit [X gleich (Y plus 7)]] [mit [Y gle=ich 3]]]))
-(kind/tex (:tex bsp6))
+(html (:hiccup bsp6))
+
 bsp6
