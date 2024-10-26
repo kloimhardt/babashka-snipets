9026c00256141c8c4200cbbbe166bbf835eb4286
kloimhardt <markus.agwin@gmx.net>
2024-10-26 16:05:30 +0200

    prototype

--- sicm_ch1.clj
+++ sicm_ch1.clj
@@ -3,19 +3,6 @@
                             numerator denominator infinite?])
   (:require [emmy.env :as e :refer :all :exclude [F->C]]))
 
-(defn define [h b]
-  (list 'defn (ffirst h)  [(second (first h))]
-        (list 'let-scheme (rest b))))
-
-(define '((L-free-particle mass) local)
-  '(let ((v (velocity local)))
-    (* 1/2 mass (dot-product v v))))
-
-(defn let-scheme [b e] (into [] (apply concat b)))
-
-(let-scheme '((v (velocity local)))
-  '(* 1/2 mass (dot-product v v)))
-
 (defn f [x]
   (let [y 1]
     (defn g [z] (+ y z))
@@ -23,3 +10,45 @@
 
 (f 7)
 (g 4)
+
+(comment
+  (defmacro hu [x] (str x))
+  ;; does not work
+  ;; (hu pi/2)
+  :end)
+
+(* 1/2 pi)
+
+(defmacro define [h b]
+  (list 'defn (ffirst h) (into [] (rest (first h)))
+        (list 'let-scheme (second b) (nth b 2))))
+
+(defmacro let-scheme [b e] (list 'let (into [] (apply concat b)) e) )
+
+(macroexpand
+  '(define ((L-free-particle mass) local)
+     (let ((v (velocity local)))
+       (* 1/2 mass (dot-product v v)))))
+
+(macroexpand
+  '(let-scheme ((v (velocity local)))
+    (* 1/2 mass (dot-product v v))))
+
+(define ((L-free-particle mass local) local)
+  (let ((v (velocity local)))
+    (* 1/2 mass (dot-product v v))))
+
+(L-free-particle 'm ['t 'x 'v])
+
+(defn hu [h b]
+  (if (coll? (first h))
+    (let [hh (hu (first h) b)]
+      (list 'def (second hh) (list 'fn (second (nth hh 2)) (list 'fn (into [] (rest h)) b))))
+    (list 'def (first h) (list 'fn (into [] (rest h)) b))))
+
+(macroexpand '(hu (a b) (identity 1)))
+;; => (def a (fn [b] (identity 1)))
+
+(hu [1 2] 5)
+(hu [[1 2] 3] 5)
+(hu [[[1 2] 3] 4] 5)
