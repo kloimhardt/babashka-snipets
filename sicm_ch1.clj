00e0d73d816654206021ee83f97d3b60003e88db
kloimhardt <markus.agwin@gmx.net>
2024-10-27 13:01:14 +0100

    finished first draft of SICM Ch1

--- sicm_ch1.clj
+++ sicm_ch1.clj
@@ -3,7 +3,8 @@
                             numerator denominator infinite?])
   (:require [emmy.env :as e :refer :all
              :exclude [F->C Lagrangian-action find-path Lagrange-equations p->r r->p
-                       Lagrangian->state-derivative Lagrange-equations-first-order]]
+                       Lagrangian->state-derivative Lagrange-equations-first-order
+                       Lagrangian->energy s->r Gamma-bar Euler-Lagrange-operator]]
             [emmy.mechanics.lagrange :as eml]
             [clojure.walk :as w]))
 
@@ -25,21 +26,26 @@
 
 (* 1/2 pi)
 
-(defmacro let-scheme [b e] (list 'let (into [] (apply concat b)) e))
+(defmacro let-scheme [b & e]
+  `(let ~(into [] (apply concat b)) ~@e))
 
-(defmacro define [h b]
+(defmacro define [h & b]
   (let [body (w/postwalk-replace {'let 'let-scheme} b)]
     (if (coll? h)
       (if (coll? (first h))
-        (list 'defn (ffirst h) (into [] (rest (first h)))
-              (list 'fn (into [] (rest h)) body))
-        (list 'defn (first h) (into [] (rest h))
-              body))
-      (list 'def h body))))
+        `(defn ~(ffirst h) ~(into [] (rest (first h)))
+           (fn ~(into [] (rest h)) ~@body))
+        `(defn ~(first h) ~(into [] (rest h))
+           ~@body))
+      `(def ~h ~@body))))
+
+(defmacro lambda [h b]
+  `(fn ~(into [] h) ~b))
 
 (def show-expression simplify)
 (def velocities velocity)
-(def  coordinates coordinate)
+(def coordinates coordinate)
+(def vector-length count)
 
 ;; 1 Lagrangian Mechanics
 ;; 1.4 Computing Actions
@@ -378,14 +384,14 @@
 
 (up 1 (up 'v_x 'v_y) (up (/ (* -1 'k 'x) 'm) (/ (* -1 'k 'y) 'm)))
 
+(define ((qv->state-path q v) t)
+  (up t (q t) (v t)))
+
 (define ((Lagrange-equations-first-order L) q v)
   (let ((state-path (qv->state-path q v)))
     (- (D state-path)
        (compose (Lagrangian->state-derivative L)
                 state-path))))
-
-(define ((qv->state-path q v) t)
-  (up t (q t) (v t)))
 
 (show-expression
  (((Lagrange-equations-first-order (L-harmonic 'm 'k))
@@ -396,3 +402,233 @@
   't))
 
 ;; Numerical integration
+
+((state-advancer harmonic-state-derivative 2.0 1.0)
+ (up 1.0 (up 1.0 2.0) (up 3.0 4.0))
+ 10.0
+ 1.0e-12)
+
+
+(up 11.0
+    (up 3.7127916645844437 5.420620823651583)
+    (up 1.6148030925459782 1.8189103724750855))
+
+
+(define ((periodic-drive amplitude frequency phase) t)
+  (* amplitude (cos (+ (* frequency t) phase))))
+
+
+(define (L-periodically-driven-pendulum m l g A omega)
+  (let ((ys (periodic-drive A omega 0)))
+    (L-pend m l g ys)))
+
+;; error
+;; No method in multimethod 'cos' for dispatch value: [:emmy.structure/up]
+;; probably lacks a require
+
+#_(show-expression
+  (((Lagrange-equations
+      (L-periodically-driven-pendulum 'm 'l 'g 'A 'omega))
+    (literal-function 'theta))
+   't))
+
+
+(define (pend-state-derivative m l g A omega)
+  (Lagrangian->state-derivative
+    (L-periodically-driven-pendulum m l g A omega)))
+
+;; error
+;; same a above
+#_(show-expression
+  ((pend-state-derivative 'm 'l 'g 'A 'omega)
+   (up 't 'theta 'thetadot)))
+
+;; 1.8 Conserved Quantities
+;; 1.8.2 Energy Conservation
+
+(define (Lagrangian->energy L)
+  (let ((P ((partial 2) L)))
+    (- (* P velocity) L)))
+
+;; 1.8.3 Central Forces in Three Dimensions
+
+(define ((T3-spherical m) state)
+  (let ((q (coordinate state))
+        (qdot (velocity state)))
+    (let ((r (ref q 0))
+          (theta (ref q 1))
+          (rdot (ref qdot 0))
+          (thetadot (ref qdot 1))
+          (phidot (ref qdot 2)))
+      (* 1/2 m
+         (+ (square rdot)
+            (square (* r thetadot))
+            (square (* r (sin theta) phidot)))))))
+
+(define (L3-central m Vr)
+  (define (Vs state)
+    (let ((r (ref (coordinate state) 0))) (Vr r)))
+  (- (T3-spherical m) Vs))
+
+(show-expression
+  (((partial 1) (L3-central 'm (literal-function 'V)))
+   (up 't
+       (up 'r 'theta 'phi)
+       (up 'rdot 'thetadot 'phidot))))
+
+(show-expression
+  (((partial 2) (L3-central 'm (literal-function 'V)))
+   (up 't
+       (up 'r 'theta 'phi)
+       (up 'rdot 'thetadot 'phidot))))
+
+(define ((ang-mom-z m) rectangular-state)
+  (let ((xyz (coordinate rectangular-state))
+        (v (velocity rectangular-state)))
+    (ref (cross-product xyz (* m v)) 2)))
+
+
+(define (s->r spherical-state)
+  (let ((q (coordinate spherical-state)))
+    (let ((r (ref q 0))
+          (theta (ref q 1))
+          (phi (ref q 2)))
+      (let ((x (* r (sin theta) (cos phi)))
+            (y (* r (sin theta) (sin phi)))
+            (z (* r (cos theta))))
+        (up x y z)))))
+
+
+(show-expression
+  ((compose (ang-mom-z 'm) (F->C s->r))
+   (up 't
+       (up 'r 'theta 'phi)
+       (up 'rdot 'thetadot 'phidot))))
+
+
+(show-expression
+  ((Lagrangian->energy (L3-central 'm (literal-function 'V)))
+   (up 't
+       (up 'r 'theta 'phi)
+       (up 'rdot 'thetadot 'phidot))))
+
+;; 1.8.4 The Restricted Three-Body Problem
+
+(define ((L0 m V) local)
+  (let ((t (time local))
+        (q (coordinates local))
+        (v (velocities local)))
+    (- (* 1/2 m (square v)) (V t q))))
+
+(define ((V a GM0 GM1 m) t xy)
+  (let ((Omega (sqrt (/ (+ GM0 GM1) (expt a 3))))
+        (a0 (* (/ GM1 (+ GM0 GM1)) a))
+        (a1 (* (/ GM0 (+ GM0 GM1)) a)))
+    (let ((x (ref xy 0)) (y (ref xy 1))
+          (x0 (* -1 a0 (cos (* Omega t))))
+          (y0 (* -1 a0 (sin (* Omega t))))
+          (x1 (* +1 a1 (cos (* Omega t))))
+          (y1 (* +1 a1 (sin (* Omega t)))))
+      (let ((r0
+              (sqrt (+ (square (- x x0)) (square (- y y0)))))
+            (r1
+              (sqrt (+ (square (- x x1)) (square (- y y1))))))
+        (- (+ (/ (* GM0 m) r0) (/ (* GM1 m) r1)))))))
+
+(define ((LR3B m a GM0 GM1) local)
+  (let ((q (coordinates local))
+        (qdot (velocities local))
+        (Omega (sqrt (/ (+ GM0 GM1) (expt a 3))))
+        (a0 (* (/ GM1 (+ GM0 GM1)) a))
+        (a1 (* (/ GM0 (+ GM0 GM1)) a)))
+    (let ((x (ref q 0))     (y (ref q 1))
+          (xdot (ref qdot 0)) (ydot (ref qdot 1)))
+      (let ((r0 (sqrt (+ (square (+ x a0)) (square y))))
+            (r1 (sqrt (+ (square (- x a1)) (square y)))))
+        (+ (* 1/2 m (square qdot))
+           (* 1/2 m (square Omega) (square q))
+           (* m Omega (- (* x ydot) (* xdot y)))
+           (/ (* GM0 m) r0) (/ (* GM1 m) r1))))))
+
+(define ((LR3B1 m a0 a1 Omega GM0 GM1) local)
+  (let ((q (coordinates local))
+        (qdot (velocities local)))
+    (let ((x (ref q 0))     (y (ref q 1))
+          (xdot (ref qdot 0)) (ydot (ref qdot 1)))
+      (let ((r0 (sqrt (+ (square (+ x a0)) (square y))))
+            (r1 (sqrt (+ (square (- x a1)) (square y)))))
+        (+ (* 1/2 m (square qdot))
+           (* 1/2 m (square Omega) (square q))
+           (* m Omega (- (* x ydot) (* xdot y)))
+           (/ (* GM0 m) r0) (/ (* GM1 m) r1))))))
+
+((Lagrangian->energy (LR3B1 'm 'a_0 'a_1 'Omega 'GM_0 'GM_1))
+ (up 't (up 'x_r 'y_r) (up 'vx_r 'vy_r)))
+
+(+ (* 1/2 'm (expt 'vx_r 2))
+   (* 1/2 'm (expt 'vy_r 2))
+   (/ (* -1 'GM_0 'm)
+      (sqrt (+ (expt (+ 'x_r 'a_0) 2) (expt 'y_r 2))))
+   (/ (* -1 'GM_1 'm)
+      (sqrt (+ (expt (- 'x_r 'a_1) 2) (expt 'y_r 2))))
+   (* -1/2 'm (expt 'Omega 2) (expt 'x_r 2))
+   (* -1/2 'm (expt 'Omega 2) (expt 'y_r 2)))
+
+;; 1.8.5 Noether’s Theorem
+
+(define (F-tilde angle-x angle-y angle-z)
+  (compose (Rx angle-x) (Ry angle-y) (Rz angle-z) coordinate))
+
+;; L-central-rectangular is already defined in chapter 1.6
+#_(define ((L-central-rectangular m U) state)
+  (let ((q (coordinate state))
+        (v (velocity state)))
+    (- (* 1/2 m (square v))
+       (U (sqrt (square q))))))
+
+(define the-Noether-integral
+  (let ((L (L-central-rectangular
+             'm (literal-function 'U))))
+    (* ((partial 2) L) ((D F-tilde) 0 0 0))))
+
+(show-expression
+  (the-Noether-integral
+    (up 't
+        (up 'x 'y 'z)
+        (up 'vx 'vy 'vz))))
+
+(down (+ (* 'm 'vy 'z) (* -1 'm 'vz 'y))
+      (+ (* 'm 'vz 'x) (* -1 'm 'vx 'z))
+      (+ (* 'm 'vx 'y) (* -1 'm 'vy 'x)))
+
+;; 1.9 Abstraction of Path Functions
+
+(define ((Gamma-bar f-bar) local)
+  ((f-bar (osculating-path local)) (time local)))
+
+(define (F->C1 F)
+  (define (C local)
+    (let ((n (vector-length local)))
+      (define (f-bar q-prime)
+        (define q
+          (compose F (Gamma q-prime)))
+        (Gamma q n))
+      ((Gamma-bar f-bar) local)))
+  C)
+
+(define (Euler-Lagrange-operator L)
+  (- (Dt ((partial 2) L)) ((partial 1) L)))
+
+((Euler-Lagrange-operator
+   (L-harmonic 'm 'k))
+ (up 't 'x 'v 'a))
+
+(+ (* 'a 'm) (* 'k 'x))
+
+((compose
+   (Euler-Lagrange-operator (L-harmonic 'm 'k))
+   (Gamma (literal-function 'x) 4))
+ 't)
+
+(+ (* 'k ((literal-function 'x) 't))
+   (* 'm (((expt D 2) (literal-function 'x)) 't)))
